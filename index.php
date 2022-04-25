0df66671b0a71a8d2b100f06387397cf698d25d1
Sara-Rahman <18103214@iubat.edugit config --global user.email 18103214@iubat.edugit config --global user.email 18103214@iubat.edu>
2022-04-25 19:19:29 +0000

    initialized

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Operation Using PHP</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Sara</td>
+                <td>Mirpur</td>
+                <td>
+                    <a href="#">Edit</a>
+                </td>
+                <td>
+                    <a href="#">Delete</a>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+    
+</body>
+<form>
+    <div class="input-group">
+        <label for=""></label>
+    </div>
+</form>
+</html>
